d896e6616b0ee1bda6d378c8c5df9de32b0d5243
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-20 08:36:06 -0500

    vault: deploy goldfish as a UI

---
+++ kubernetes/goldfish/vault-policies/goldfish.hcl
@@ -0,0 +1,3 @@
+path "secret/goldfish" {
+  capabilities = ["read", "update"]
+}
--- kubernetes/pull-private.sh
+++ kubernetes/pull-private.sh
@@ -1,3 +1,4 @@
 eval $(minikube docker-env)
 docker pull gcr.io/the-psf/certificate-requestor:v1.0.0a1
 docker pull gcr.io/the-psf/certificate-approver:v1.0.0a1
+docker pull gcr.io/the-psf/vault-enrollment-controller:v1.0.0a1
