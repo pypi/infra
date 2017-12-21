65de2e1712a1568fb23087f22205e59dbab980bf
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-20 22:54:14 -0500

    general cleanup
    - restructure placement of vault-auth-kubernetes-plugin bootstrap
    - add dockerignore files
    - add a sketch of a "setup script"

--- kubernetes/pull-private.sh
+++ kubernetes/pull-private.sh
@@ -2,3 +2,5 @@ eval $(minikube docker-env)
 docker pull gcr.io/the-psf/certificate-requestor:v1.0.0a1
 docker pull gcr.io/the-psf/certificate-approver:v1.0.0a1
 docker pull gcr.io/the-psf/vault-enrollment-controller:v1.0.0a1
+docker pull gcr.io/the-psf/secure-sidecar:v1.0.0a1
+docker pull gcr.io/the-psf/goldfish:v1.0.0a1
---
+++ kubernetes/setup-cluster.sh
@@ -0,0 +1,23 @@
+kubectl apply -f certificate-approver/manifests
+kubectl apply -f certificate-requestor/manifests
+kubectl create ns vault
+kubectl apply -f consul/manifests
+pushd consul
+./bootstrap-acls 
+popd
+pushd vault
+kubectl apply -f manifests/initial-secrets.yaml
+export CONSUL_MANAGEMENT_TOKEN=55961ec2-3290-ab00-45e2-a73274f69022
+./bootstrap-acls 
+kubectl apply -f manifests/vault.yaml 
+./bootstrap-vault 
+export VAULT_TOKEN=55c52460-23ad-42d5-5b62-81724fe17b2f
+popd
+pushd vault-enrollment-controller/
+kubectl apply -f manifests
+./bootstrap-vault-kubernetes-auth 
+popd
+pushd goldfish/
+./boostrap-vault-goldfish 
+kubectl apply -f manifests
+popd
