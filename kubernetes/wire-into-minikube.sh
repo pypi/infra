4c11cf5d60d09122dc7d498df4ac346a3bff6637
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-22 16:21:57 -0500

    minikube: setup access to cluster resources from host machine

--- kubernetes/wire-into-minikube.sh
+++ kubernetes/wire-into-minikube.sh
@@ -35,12 +35,27 @@ search svc.cluster.local default.svc.cluster.local
 options ndots:5
 EOF'
 
-for interface in $(ifconfig 'bridge0' | grep member | awk '{print $2}'); do
-    echo "Adding hostfilter to bridge0 for $interface"
-    sudo ifconfig bridge0 -hostfilter $interface
-done
+#for interface in $(ifconfig 'bridge0' | grep member | awk '{print $2}'); do
+#    echo "Adding hostfilter to bridge0 for $interface"
+#    sudo ifconfig bridge0 -hostfilter $interface
+#done
 
 echo "Deleting any existing routes for 10.96.0.0/12"
 sudo route -n delete 10.96/12 > /dev/null 2>&1
 echo "Adding for 10.96.0.0/12 via $(minikube ip)"
-sudo route -n add 10.96.0.0/24 $(minikube ip) > /dev/null 2>&1
+sudo route -n add 10.96.0.0/12 $(minikube ip) > /dev/null 2>&1
+
+echo "Preparing pkcs12 bundle for TLS client auth"
+openssl pkcs12 -export -clcerts -in $HOME/.minikube/client.crt -inkey $HOME/.minikube/client.key -out $HOME/.minikube/client.p12 -passout pass:minikube
+
+echo "Importing minikubeCA into your login Keychain"
+echo "You'll be prompted for your administrator password"
+set -x
+security add-trusted-cert -d -r trustRoot -k $HOME/Library/Keychains/login.keychain $HOME/.minikube/ca.crt
+set +x
+
+echo "Importing minikube client key and cert to your login Keychain"
+echo "When prompted for "
+set -x
+security import $HOME/.minikube/client.p12 -k $HOME/Library/Keychains/login.keychain
+set +x
