544d097e19bf1716bcd27e7dcd2d5d76ae8b4f81
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-23 13:23:31 -0500

    minikube: give docker more file descriptors

--- kubernetes/start-minikube.sh
+++ kubernetes/start-minikube.sh
@@ -2,6 +2,7 @@ minikube start \
   --memory=4096 \
   --kubernetes-version v1.8.4 \
   --bootstrapper kubeadm \
+  --docker-opt="default-ulimit=nofile=102400:102400" \
   --extra-config=controller-manager.cluster-signing-cert-file="/var/lib/localkube/certs/ca.crt" \
   --extra-config=controller-manager.cluster-signing-key-file="/var/lib/localkube/certs/ca.key"
 
