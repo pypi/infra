f2db939d02e0b473c661afe6d25e3dc9c8a1a6db
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-12-28 19:25:56 -0500

    minikube: update "setup script"

--- kubernetes/scripts/setup-cluster.sh
+++ kubernetes/scripts/setup-cluster.sh
@@ -1,23 +1,24 @@
-kubectl apply -f certificate-approver/manifests
-kubectl apply -f certificate-requestor/manifests
-kubectl create ns vault
-kubectl apply -f consul/manifests
-pushd consul
-./bootstrap-acls 
-popd
-pushd vault
-export CONSUL_MANAGEMENT_TOKEN=55961ec2-3290-ab00-45e2-a73274f69022
-./bootstrap-acls 
-kubectl apply -f manifests/vault.yaml 
-./bootstrap-vault 
-export VAULT_TOKEN=55c52460-23ad-42d5-5b62-81724fe17b2f
-popd
-pushd vault-enrollment-controller/
-kubectl apply -f manifests
-./bootstrap-vault-kubernetes-auth 
-./bootstrap-vault-ca-minikube
-popd
-pushd goldfish/
-./boostrap-vault-goldfish 
-kubectl apply -f manifests
-popd
+#!/bin/bash
+
+echo "not quite ready for direct execution yet..."
+exit 1
+
+./scripts/start-minikube
+./scripts/pull-private
+kubectl apply -f manifests/kube-system/certificate-approver
+kubectl apply -f manifests/kube-system/certificate-requestor
+kubectl apply -f manifests/cabotage/00-namespace.yml
+kubectl apply -f manifests/cabotage/consul
+unset CONSUL_MANAGEMENT_TOKEN
+./bootstrap-scripts/cabotage/consul/bootstrap-acls
+export CONSUL_MANAGEMENT_TOKEN=2dd53a07-1c6c-8e64-8c9e-50f273e25a1f
+./bootstrap-scripts/cabotage/vault/bootstrap-acls
+kubectl apply -f manifests/cabotage/vault
+unset VAULT_TOKEN
+./bootstrap-scripts/cabotage/vault/bootstrap-vault
+export VAULT_TOKEN=69f5a390-a444-abe4-605f-5a4831140588
+./bootstrap-scripts/cabotage/enrollment-controller/bootstrap-vault-kubernetes-auth
+./bootstrap-scripts/cabotage/enrollment-controller/bootstrap-vault-ca-minikube
+kubectl apply -f manifests/cabotage/enrollment-controller
+./bootstrap-scripts/cabotage/goldfish/boostrap-vault-goldfish
+kubectl apply -f manifests/cabotage/goldfish
