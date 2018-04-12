db244d753ad52fe297024a89f9596bc351133140
Donald Stufft <donald@stufft.io>
2018-04-12 17:08:42 -0400

    Setup hosting for files.pythonhosted.org in Terraform (#9)

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,12 +1,21 @@
+variable "credentials" { type = "map" }
+
+
 provider "aws" {
   region  = "us-east-2"
   profile = "psf-prod"
 }
 
+
 provider "aws" {
   alias   = "us-west-2"
   region  = "us-west-2"
   profile = "psf-prod"
+}
+
+
+provider "fastly" {
+  api_key = "${var.credentials["fastly"]}"
 }
 
 
