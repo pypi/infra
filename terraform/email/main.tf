c1837efaa1383c82e90049fafdcb6f0d1215b813
Donald Stufft <donald@stufft.io>
2018-04-11 20:29:08 -0400

    Wire up the SES domain to our SNS topics

--- terraform/email/main.tf
+++ terraform/email/main.tf
@@ -1,5 +1,8 @@
 provider "aws" {}
-provider "aws" { alias = "email" }
+provider "aws" {
+    version = "~> 1.14"
+    alias = "email"
+}
 
 
 variable "domain" { type = "string" }
@@ -71,13 +74,35 @@ resource "aws_sns_topic_subscription" "delivery-events" {
 }
 
 
-# TODO: We can't setup the default sending policy for a SES domain yet, because this
-#       functionality doesn't exist in Terraform yet. THere is an open issue to add
-#       this (https://github.com/terraform-providers/terraform-provider-aws/issues/931)
-#       along with a PR that does the actuak work
-#       (https://github.com/terraform-providers/terraform-provider-aws/pull/2640).
-#       However, until those get added we're going to have to manually configure the
-#       SES domain to send the requisete events to our SNS topic.
+resource "aws_ses_identity_notification_topic" "primary-deliveries" {
+  provider          = "aws.email"
+  topic_arn         = "${aws_sns_topic.delivery-events.arn}"
+  notification_type = "Delivery"
+  identity          = "${aws_ses_domain_identity.primary.domain}"
+}
+
+
+resource "aws_ses_identity_notification_topic" "primary-bounces" {
+  provider          = "aws.email"
+  topic_arn         = "${aws_sns_topic.delivery-events.arn}"
+  notification_type = "Bounce"
+  identity          = "${aws_ses_domain_identity.primary.domain}"
+}
+
+
+resource "aws_ses_identity_notification_topic" "primary-complaints" {
+  provider          = "aws.email"
+  topic_arn         = "${aws_sns_topic.delivery-events.arn}"
+  notification_type = "Complaint"
+  identity          = "${aws_ses_domain_identity.primary.domain}"
+}
+
+
+# TODO: We can't disable the policy of sending emails for a SES domain yet, because
+#       this functionality doesn't exist in terraform yet. The issue to track this
+#       is https://github.com/terraform-providers/terraform-provider-aws/issues/4182.
+#       However, until that is addressed we will have to manually configure the
+#       SES domain to disable email notifications for bounces and complaints.
 
 
 output "delivery_topic" { value = "${aws_sns_topic.delivery-events.arn}" }
