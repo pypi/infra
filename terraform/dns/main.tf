585c11303430cf3a0bbc96d4faf24f7108064b0c
Donald Stufft <donald@stufft.io>
2018-04-12 02:57:33 -0400

    Wire up pythonhosted.org into terraform (#7)

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,13 +1,22 @@
-variable "domain" {
-  type = "string"
-}
+variable "primary_domain" { type = "string" }
+variable "user_content_domain" { type = "string" }
+
+
+resource "aws_route53_delegation_set" "ns" {}
 
 
 resource "aws_route53_zone" "primary" {
-  name = "${var.domain}"
+  name              = "${var.primary_domain}"
+  delegation_set_id = "${aws_route53_delegation_set.ns.id}"
 }
 
 
-output "zone_id" {
-  value = "${aws_route53_zone.primary.zone_id}"
+resource "aws_route53_zone" "user_content" {
+  name = "${var.user_content_domain}"
+  delegation_set_id = "${aws_route53_delegation_set.ns.id}"
 }
+
+
+output "nameservers" { value = ["${aws_route53_delegation_set.ns.name_servers}"] }
+output "primary_zone_id" { value = "${aws_route53_zone.primary.zone_id}" }
+output "user_content_zone_id" { value = "${aws_route53_zone.user_content.zone_id}" }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 module "dns" {
-    source = "./dns"
-    domain = "pypi.org"
+  source = "./dns"
+
+  primary_domain = "pypi.org"
+  user_content_domain = "pythonhosted.org"
 }
 
 
@@ -11,9 +13,11 @@ module "email" {
     "aws.email" = "aws.us-west-2"
   }
 
-  zone_id  = "${module.dns.zone_id}"
+  zone_id  = "${module.dns.primary_zone_id}"
   domain   = "pypi.org"
   hook_url = "https://pypi.org/_/ses-hook/"
 }
 
+
+output "nameservers" { value = ["${module.dns.nameservers}"] }
 output "ses_delivery_topic" { value = "${module.email.delivery_topic}" }
