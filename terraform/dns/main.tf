0bdde85a9a696adc47d0724149371c6b0889ae6a
Donald Stufft <donald@stufft.io>
2018-08-09 11:50:52 -0400

    Add CAA records

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,6 +1,8 @@
 variable "tags" { type = "map" }
 variable "primary_domain" { type = "string" }
 variable "user_content_domain" { type = "string" }
+variable "caa_report_uri" { type = "string" }
+variable "caa_issuers" { type = "list" }
 variable "apex_txt" { type = "list" }
 
 
@@ -13,6 +15,13 @@ resource "aws_route53_zone" "primary" {
   tags              = "${var.tags}"
 }
 
+resource "aws_route53_record" "caa" {
+    zone_id = "${aws_route53_zone.primary.zone_id}"
+    name    = "${var.primary_domain}"
+    type    = "CAA"
+    ttl     = 60
+    records = "${concat(formatlist("0 issue \"%s\"", var.caa_issuers), list("0 iodef \"${var.caa_report_uri}\""))}"
+}
 
 resource "aws_route53_record" "apex_txt" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
@@ -27,6 +36,14 @@ resource "aws_route53_zone" "user_content" {
   name              = "${var.user_content_domain}"
   delegation_set_id = "${aws_route53_delegation_set.ns.id}"
   tags              = "${var.tags}"
+}
+
+resource "aws_route53_record" "user_content_caa" {
+    zone_id = "${aws_route53_zone.user_content.zone_id}"
+    name    = "${var.user_content_domain}"
+    type    = "CAA"
+    ttl     = 60
+    records = "${concat(formatlist("0 issue \"%s\"", var.caa_issuers), list("0 iodef \"${var.caa_report_uri}\""))}"
 }
 
 
