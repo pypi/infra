8417388d4e709fc4dd18f711526d05dac1ba542a
Ernest W Durbin III <ewdurbin@gmail.com>
2020-01-08 12:16:36 -0500

    bump DNS ttls

--- terraform/warehouse/main.tf
+++ terraform/warehouse/main.tf
@@ -198,7 +198,7 @@ resource "aws_route53_record" "primary" {
   zone_id = "${var.zone_id}"
   name    = "${var.domain}"
   type    = "${local.apex_domain ? "A" : "CNAME"}"
-  ttl     = 60
+  ttl     = 1800
   records = ["${var.fastly_endpoints["${join("_", list(var.domain_map[var.domain], local.apex_domain ? "A" : "CNAME"))}"]}"]
 }
 
@@ -208,6 +208,6 @@ resource "aws_route53_record" "primary-ipv6" {
   zone_id = "${var.zone_id}"
   name    = "${var.domain}"
   type    = "AAAA"
-  ttl     = 60
+  ttl     = 1800
   records = ["${var.fastly_endpoints["${join("_", list(var.domain_map[var.domain], "AAAA"))}"]}"]
 }
