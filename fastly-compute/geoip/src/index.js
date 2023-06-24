8b8e2903e4d430d119f506d0fb16becac9d60afd
Ee Durbin <ewdurbin@gmail.com>
2023-06-24 08:50:21 -0400

    js-beautify

--- fastly-compute/geoip/src/index.js
+++ fastly-compute/geoip/src/index.js
@@ -1,49 +1,57 @@
 /// <reference types="@fastly/js-compute" />
 
-import { getGeolocationForIpAddress } from "fastly:geolocation"
-import { ConfigStore } from "fastly:config-store";
+import {
+    getGeolocationForIpAddress
+} from "fastly:geolocation"
+import {
+    ConfigStore
+} from "fastly:config-store";
 
 async function app(event) {
-  try {
-   const configStore = new ConfigStore('geoip_auth')
+    try {
+        const configStore = new ConfigStore('geoip_auth')
 
-   // Fetch token from X-Secret header
-   var token = event.request.headers.get('X-Secret');
-   // Check if the auth token exists in the ConfigStore
-   var auth = await configStore.get(token)
+        // Fetch token from X-Secret header
+        var token = event.request.headers.get('X-Secret');
+        // Check if the auth token exists in the ConfigStore
+        var auth = await configStore.get(token)
 
-   // If the value doesn't exist or is false, return Unauthorized
-   if (!auth) {
-     let respBody = JSON.stringify({ Error: "Unauthorized" });
-     return new Response(respBody, {
-       status: 401,
-       headers: { 'Content-Type': 'application/json' }
-     });
+        // If the value doesn't exist or is false, return Unauthorized
+        if (!auth) {
+            let respBody = JSON.stringify({
+                Error: "Unauthorized"
+            });
+            return new Response(respBody, {
+                status: 401,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }
+        let ip = new URL(event.request.url).searchParams.get('ip') || event.client.address
+        let geo = getGeolocationForIpAddress(ip);
+        let respBody = JSON.stringify({
+            geo: {
+                city: geo.city,
+                continent: geo.continent,
+                country_code: geo.country_code,
+                country_code3: geo.country_code3,
+                country_name: geo.country_name,
+                region: geo.region,
+            },
+        });
+
+        return new Response(respBody, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response("Internal Server Error", {
+            status: 500
+        });
     }
-    let ip = new URL(event.request.url).searchParams.get('ip') || event.client.address
-    let geo = getGeolocationForIpAddress(ip);
-    let respBody = JSON.stringify({
-      geo: {
-        city: geo.city,
-        continent: geo.continent,
-        country_code: geo.country_code,
-        country_code3: geo.country_code3,
-        country_name: geo.country_name,
-        region: geo.region,
-      },
-});
-
-    return new Response( respBody, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response("Internal Server Error", {
-      status: 500
-    });
-  }
 }
 
 addEventListener("fetch", (event) => event.respondWith(app(event)));
