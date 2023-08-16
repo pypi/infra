783f269201c8a7a931a454b4b36176c6039dd34c
Chloe Gerhardson <chloe.gerhardson@pyfound.org>
2023-08-16 07:59:31 -0400

    Add sni hostname config

---
+++ terraform/image-proxy/main.tf
@@ -0,0 +1,20 @@
+variable "domain" { type = string }
+variable "sitename" { type = string }
+variable "conveyor_address" { type = string }
+
+
+resource "fastly_service_vcl" "test-pypi-camo" {
+  name = var.sitename
+
+  domain {
+    name = var.domain
+  }
+  backend { 
+    address = var.conveyor_address
+    name    = var.sitename
+    port    = 443
+    ssl_cert_hostname = var.conveyor_address
+    ssl_sni_hostname  = var.conveyor_address
+  }
+}
+  
