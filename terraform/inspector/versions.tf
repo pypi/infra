7cf18ef5d7721d10b12b910e044f449f89fcd518
Jacob Coffee <jacob@z7x.org>
2025-07-25 13:52:54 -0400

    Add Inspector to NGWAF (#208)

--- terraform/inspector/versions.tf
+++ terraform/inspector/versions.tf
@@ -8,6 +8,10 @@ terraform {
     fastly = {
       source = "fastly/fastly"
     }
+    sigsci = {
+      source  = "signalsciences/sigsci"
+      version = "3.3.0"
+    }
   }
   # required_version = ">= 1.12.0"
 }
