0f72bfcd989b05d785bdfac22c4fc38c3b470aaf
Jacob Coffee <jacob@z7x.org>
2025-10-10 10:26:49 -0500

    fix: clean up things we dont use/are default

--- terraform/aws-iam/outputs.tf
+++ terraform/aws-iam/outputs.tf
@@ -2,9 +2,6 @@ output "group_arns" {
   description = "ARNs of IAM groups"
   value = {
     administrator = aws_iam_group.administrator.arn
-    billing       = aws_iam_group.billing.arn
-    kops          = aws_iam_group.kops.arn
-    readonly      = aws_iam_group.readonly.arn
   }
 }
 
@@ -18,7 +15,6 @@ output "role_arns" {
 output "policy_arns" {
   description = "ARNs of custom IAM policies"
   value = {
-    billing_full_access  = aws_iam_policy.billing_full_access.arn
     datadog_integration = aws_iam_policy.datadog_integration.arn
   }
 }
